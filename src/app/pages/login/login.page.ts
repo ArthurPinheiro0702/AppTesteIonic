acb14d6ba24cf383d37c8f6bb107dd72502de483
ArthurPinheiro <arthur.pinheiro@cerfox.com.br>
2025-11-28 11:28:36 -0300

    correção rota

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -42,7 +42,7 @@ export class LoginPage {
     if (user?.role === 'admin') {
       this.router.navigate(['/admin']);
     } else {
-      this.router.navigate(['/shop']);
+      this.router.navigate(['/loja']);
     }
   }
 
